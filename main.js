f80128f5b78ea4a4c420a5b519642716b53768e2
allenJcodes <110220645+allenJcodes@users.noreply.github.com>
2024-03-17 15:40:17 +0800

    final update

--- main.js
+++ main.js
@@ -1,33 +1,42 @@
-// let path = document.querySelector('#rose-path');
-// let pathLength = path.getTotalLength();
+const html = document.documentElement;
+const roseSvg = document.getElementById('rose-svg');
+const rosePath = document.querySelector('#rose-path');
+const rosePathLength = rosePath.getTotalLength();
+const text = document.querySelector('#text');
+text.innerHTML = 'Scroll down slowly :)';
 
-// path.style.strokeDasharray = pathLength + ' ' + pathLength;
-// path.style.strokeDasharray = pathLength;
-
-// window.addEventListener('scroll', () => {
-//   let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - window.innerHeight);
-//   let drawLength = pathLength * scrollPercentage;
-//   path.setAttribute('stroke', 'white');
-//   path.setAttribute('stroke-width', 7);
-
-//   path.style.strokeDashoffset = pathLength - drawLength;
-  
-// })
-
-let path = document.querySelector('#rose-path');
-let pathLength = path.getTotalLength();
-
-path.style.strokeDasharray = pathLength + ' ' + pathLength;
-
-path.setAttribute('stroke-width', 7) ;
+let vh = window.innerHeight * 0.01;
+// set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('scroll', () => {
-  var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - window.innerHeight);
-  var drawLength = pathLength * scrollPercentage;
-  path.setAttribute('stroke', '#DABC94');
+  // get the scroll percentage based on the position of the scroll bar to the top of the document.
+  let scrollPercentage = (html.scrollTop + document.body.scrollTop) / (html.scrollHeight - window.innerHeight);
 
-  // scrollPercentage > 0.5 ? path.setAttribute('fill', 'white'): path.setAttribute('fill', 'white');
-  console.log(drawLength);
-  path.style.strokeDashoffset = pathLength - drawLength;
-  path.style.strokeDasharray = drawLength + ' ' + pathLength;
+  // gets the length of the path multiplied by the scroll percentage
+  let drawLength = rosePathLength * scrollPercentage;
+
+  // sets the attributes of the svg element upon scrolling
+  roseSvg.setAttribute('stroke-width', 7);
+  roseSvg.setAttribute('stroke', '#DABC94');
+
+
+  // hides the text on first scroll.
+  if (scrollPercentage > 0.001 ) {
+    text.style = 'color : rgba(0, 0, 0, 0)';
+  }
+
+  // change the innerHTML depending on the scrollPercentage
+  if (scrollPercentage >= 0.27 && scrollPercentage <= 0.35) {
+    text.innerHTML = 'Happy Birthday Aevie <3';
+    text.style = 'color : rgba(255, 255, 255, 1)';
+  }
+  if (scrollPercentage >=.41 && scrollPercentage <= 0.5) {
+    text.style = 'color : rgba(255, 255, 255, 1)';
+    text.innerHTML = 'I Love You';
+  }
+  
+  // changes the strokeDashOffset and strokeDashArray properties
+  rosePath.style.strokeDasharray = drawLength+ ' ' + rosePathLength;
+  rosePath.style.strokeDashoffset = rosePathLength - drawLength;
 })
